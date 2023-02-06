4de07a7f719882a9866d91ceef45a2776776e726
ucan-lab <ucan-lab@users.noreply.github.com>
2023-02-06 06:22:33 +0000

    Apply Pint

--- src/app/Http/Controllers/SampleController.php
+++ src/app/Http/Controllers/SampleController.php
@@ -2,13 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
 class SampleController extends Controller
 {
     public function __invoke(): array
     {
         return [];
     }
-
 }
